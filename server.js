dd4fc14f3eb4fef17a0e7a61156fa4edfc15e1c3
Elianice Gorniak <eligorniak@gmail.com>
2019-09-25 14:43:04 -0500

    updated files for Heroku deployment

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // Starts the server to begin listening (using arrow function):
 app.listen(PORT, () => {
